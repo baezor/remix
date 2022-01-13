4910e50106976447d150ae5c006a00856ca35c44
baezor <baezbz@gmail.com>
2022-01-13 09:31:10 -0600

    🖊️  Updated content

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,32 +1,10 @@
 export default function Index() {
   return (
     <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.4" }}>
-      <h1>Welcome to Remix</h1>
-      <ul>
-        <li>
-          <a
-            target="_blank"
-            href="https://remix.run/tutorials/blog"
-            rel="noreferrer"
-          >
-            15m Quickstart Blog Tutorial
-          </a>
-        </li>
-        <li>
-          <a
-            target="_blank"
-            href="https://remix.run/tutorials/jokes"
-            rel="noreferrer"
-          >
-            Deep Dive Jokes App Tutorial
-          </a>
-        </li>
-        <li>
-          <a target="_blank" href="https://remix.run/docs" rel="noreferrer">
-            Remix Docs
-          </a>
-        </li>
-      </ul>
+      <h1>Angel Romero Baez</h1>
+      <p>Hey! I'm Angel! This site is still under construction.</p>
+      <p>Thanks for visiting</p>
+      <p>Reach me out at <a href="mailto:angel@romerobaez.com">angel@romerobaez.com</a></p>
     </div>
   );
 }
